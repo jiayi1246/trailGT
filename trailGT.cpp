35f9c6897fabdf4c2b819ce23fa2179afb057f9c
Christopher Rasmussen <cer@cis.udel.edu>
2016-06-07 16:48:49 -0400

    advance to next most isolated non-vert image; display improvements

--- trailGT.cpp
+++ trailGT.cpp
@@ -21,22 +21,34 @@ using namespace cv;
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 
-void add_all_images();
 void add_images(string, vector <string> &);
+void add_all_images_from_file(string);
 void draw_overlay();
 void onMouse(int, int, int, int, void *);
 
 void saveVert(); int loadVert();
 void saveBad(); void loadBad();
 
+int most_isolated_nonvert_image_idx();
+int most_isolated_nonvert_image_idx(int);
+
+bool isBad(int);
+bool isVert(int);
+
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 
+#define IMAGE_ROW_FAR    100
+#define IMAGE_ROW_NEAR   175
+#define ZERO_INDEX       0
 #define BIG_INDEX_STEP   10
 #define REQUIRED_NUMBER_OF_VERTS_PER_IMAGE   4
+#define NO_INDEX         -1
+#define NO_DISTANCE      -1
+#define COMMENT_CHAR     '#'
 
-string imdirname = "/warthog_logs/Aug_05_2011_Fri_11_50_24_AM/omni_images/";
-string fname;
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 vector <string> dir_image_filename;  // full-path image filenames
  
@@ -46,16 +58,21 @@ vector <int> Random_idx;         // R[i] holds random index
 vector <int> Nonrandom_idx;      // R[N[i]] = i
 vector < vector <Point> > Vert;  // actual trail vertices for each image 
 
+vector <int> ClosestVert_dist;   // for NoVert images, how many indices away is the *nearest* Vert image?
+                                 // max of this is "most isolated"
+
 set <int> Bad_idx_set;           // which images are bad
 set <int> Vert_idx_set;          // which images have edge vertex info
 set <int> NoVert_idx_set;        // which images do NOT have edge vertex info (Vert U NoVert = all images)
 
 bool do_random = false;
+bool do_verts = false;
 bool do_overlay = true;
 bool do_bad = false;
 int bad_start, bad_end;          // indices of bad range
 
 bool bad_current_index = false;
+bool vert_current_index = false;
 
 bool callbacks_set = false;
 bool dragging = false;
@@ -67,17 +84,16 @@ double fontScale = 0.35;
 vector <int> trailEdgeRow;
 
 int num_saved_verts = 0;
-int current_index = 0;
+int current_index;
 string current_imname;
 
-//----------------------------------------------------------------------------
-
-// if being run for first time: 
-//  -- go through directories and gather image names
-//  -- write some kinda database file with names, "empty" GT info (serialize map with OpenCV's XML output capabilities?) 
-// if database file exists, read it and print info (how many images, how many have GT)
+int max_closest_vert_dist;
+int next_nonvert_idx;
 
 //----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+// change which image we are currently processing/displaying
 
 void set_current_index(int new_index)
 {
@@ -103,7 +119,12 @@ void set_current_index(int new_index)
 
   // is this a "bad" image?
 
-  bad_current_index = (Bad_idx_set.find(current_index) != Bad_idx_set.end());  
+  bad_current_index = isBad(current_index);
+
+  // do we have vertices for this image?
+
+  vert_current_index = isVert(current_index);
+
 }
 
 //----------------------------------------------------------------------------
@@ -133,14 +154,24 @@ void add_images(string dirname, vector <string> & imnames)
 
 //----------------------------------------------------------------------------
 
-void add_all_images()
-{
-  add_images(string("/warthog_logs/Aug_05_2011_Fri_11_50_24_AM/omni_images/"), dir_image_filename);
-  add_images(string("/warthog_logs/Aug_05_2011_Fri_11_54_36_AM/omni_images/"), dir_image_filename);
-  add_images(string("/warthog_logs/Aug_05_2011_Fri_12_25_11_PM/omni_images/"), dir_image_filename);
-  add_images(string("/warthog_logs/Aug_05_2011_Fri_12_29_30_PM/omni_images/"), dir_image_filename);
+// *each line* of file loaded here should have the full path to a directory full of numbered images 
 
-  //  add_images(string("./test_omni_images/"), dir_image_filename);
+void add_all_images_from_file(string impathfilename)
+{
+  ifstream inStream;
+  string line;
+
+  // read file
+
+  inStream.open(impathfilename.c_str());
+
+  while (getline(inStream, line)) {
+
+    cout << "loading " << line << endl;
+
+    add_images(line, dir_image_filename);
+  }
+
 
   sort(dir_image_filename.begin(), dir_image_filename.end());
 }
@@ -166,6 +197,8 @@ int snap_y(int y)
 }
 
 //----------------------------------------------------------------------------
+
+// what to do when mouse is moved/mouse button is push/released
 
 void onMouse(int event, int x, int y, int flags, void *userdata)
 {
@@ -247,6 +280,9 @@ void onMouse(int event, int x, int y, int flags, void *userdata)
       if (iter != NoVert_idx_set.end())
 	NoVert_idx_set.erase(iter);
 
+      vert_current_index = true;
+
+      next_nonvert_idx = most_isolated_nonvert_image_idx(current_index);
     }
 
   }
@@ -273,6 +309,11 @@ void onMouse(int event, int x, int y, int flags, void *userdata)
       Vert_idx_set.erase(iter);
 
     NoVert_idx_set.insert(current_index);
+
+    vert_current_index = false;
+
+    // this could be done more efficiently, but it should be a pretty rare event
+    next_nonvert_idx = most_isolated_nonvert_image_idx();
   }
 
   draw_im = current_im.clone();
@@ -293,6 +334,8 @@ void onMouse(int event, int x, int y, int flags, void *userdata)
 
 //----------------------------------------------------------------------------
 
+// what to do when a key is pressed
+
 void onKeyPress(char c)
 {
   int idx;
@@ -302,7 +345,16 @@ void onKeyPress(char c)
   
   if (c == '0') {
 
-    set_current_index(0);
+    set_current_index(ZERO_INDEX);
+
+  }
+
+  // most isolated nonvert image remaining
+
+  else if (c == 'n') {
+
+    saveVert();   // for speed
+    set_current_index(next_nonvert_idx);
 
   }
 
@@ -315,16 +367,17 @@ void onKeyPress(char c)
     
     if (!do_random) {
 
-      set_current_index(current_index + step_idx);
+      if (do_verts) {
+	set<int>::iterator iter = Vert_idx_set.find(current_index);
+	if ((iter != Vert_idx_set.end()) && (iter == --Vert_idx_set.end()))
+	  iter = Vert_idx_set.begin();
+	else
+	  iter++;
+	set_current_index(*iter);
+      }
+      else
+	set_current_index(current_index + step_idx);
 
-      /*
-      current_index += step_idx;  
-      
-      // wrap?
-      
-      if (current_index >= dir_image_filename.size())
-	current_index = 0;
-      */
     }
     else {
       idx = Nonrandom_idx[current_index] + step_idx;  
@@ -332,7 +385,6 @@ void onKeyPress(char c)
       if (idx >= dir_image_filename.size())
 	idx = 0;
 
-      //      current_index = Random_idx[idx];
       set_current_index(Random_idx[idx]);
     }
 
@@ -347,16 +399,20 @@ void onKeyPress(char c)
 
     if (!do_random) {
 
-      set_current_index(current_index - step_idx);
-
-      /*
-      current_index -= step_idx;
+      if (do_verts) {
+	set<int>::iterator iter = Vert_idx_set.find(current_index);
+	if (iter != Vert_idx_set.begin())
+	  iter--;
+	else {
+	  iter = Vert_idx_set.end();
+	  --iter;
+	}
+	set_current_index(*iter);
+	
+      }
+      else
+	set_current_index(current_index - step_idx);
       
-      // wrap?
-      
-      if (current_index < 0)
-	current_index = dir_image_filename.size() - 1;
-      */
     }
     else {
       idx = Nonrandom_idx[current_index] - step_idx;  
@@ -364,7 +420,6 @@ void onKeyPress(char c)
       if (idx < 0)
 	idx = dir_image_filename.size() - 1;
 
-      //      current_index = Random_idx[idx];
       set_current_index(Random_idx[idx]);
     }   
   }
@@ -376,8 +431,20 @@ void onKeyPress(char c)
   
   // toggle randomized index mode
   
-  else if (c == 'r') 
-    do_random = !do_random;
+  else if (c == 'r') {
+    if (!do_verts)
+      do_random = !do_random;
+  }
+
+  // toggle vert image-only mode
+  
+  else if (c == 'v') {
+    if (vert_current_index) {
+      do_verts = !do_verts;
+      if (do_verts)
+	do_random = false;
+    }
+  }
 
   // this is a bad image (no trail or trail geometry is wacky)
   
@@ -404,7 +471,7 @@ void onKeyPress(char c)
     bad_current_index = false;
   }
 
-  // save
+  // save everything
 
   else if (c == 's') {
     saveVert();
@@ -438,26 +505,39 @@ void setChannel(Mat &mat, unsigned int channel, unsigned char value)
 
 //----------------------------------------------------------------------------
 
+// draw strings and shapes on top of current image
+
 void draw_overlay()
 {
+  stringstream ss;
+
   if (do_overlay) {
     
     // which image is this?
 
-    stringstream ss;
     ss << current_index << ": " << current_imname;
     string str = ss.str();
 
     putText(draw_im, str, Point(5, 10), FONT_HERSHEY_SIMPLEX, fontScale, Scalar::all(255), 1, 8);
+
+    // isolation stats
+
+    if (!bad_current_index && !vert_current_index) {
+      ss.str("");
+      ss << "max dist = " << max_closest_vert_dist << ", this dist = " << ClosestVert_dist[current_index];
+      putText(draw_im, ss.str(), Point(5, 25), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(255, 255, 255), 1, 8);
+    }
+
+    // save status
 
     int num_unsaved = Vert_idx_set.size() - num_saved_verts;
 
     if (num_unsaved > 0) {
       ss.str("");
       if (num_unsaved == 1)
-	ss << num_unsaved << " image with unsaved verts";
+	ss << num_unsaved << " image with unsaved verts [" << Vert_idx_set.size() << "]";
       else
-	ss << num_unsaved << " images with unsaved verts";
+	ss << num_unsaved << " images with unsaved verts [" << Vert_idx_set.size() << "]";
       str = ss.str();
       putText(draw_im, str, Point(5, 315), FONT_HERSHEY_SIMPLEX, 1.5 * fontScale, Scalar(0, 0, 255), 1, 8);
     }
@@ -465,19 +545,30 @@ void draw_overlay()
     // are we in "random next image" mode?
 
     if (do_random) 
-      putText(draw_im, "R", Point(5, 25), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 1, 8);
+      putText(draw_im, "R", Point(5, 40), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 1, 8);
 
     // are we in "bad image" marking mode?
 
     if (do_bad) 
-      putText(draw_im, "B", Point(15, 25), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 1, 8);
+      putText(draw_im, "B", Point(15, 40), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 1, 8);
 
-    // is *this* "bad" image?
+    // are we in "verts only" mode?
+
+    if (do_verts) 
+      putText(draw_im, "V", Point(25, 40), FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 1, 8);
+
+    // is this a "bad" image?
 
     if (bad_current_index) {
       setChannel(draw_im, 2, 255);
       return;
     }
+
+    // is this an image for which we have ground-truth trail edges?
+
+    else if (vert_current_index) 
+      setChannel(draw_im, 0, 200);
+   
 
     // horizontal lines for trail edge rows
 
@@ -495,10 +586,10 @@ void draw_overlay()
       r = 0; g = 255;
     }
 
-    //    if (!(Vert[current_index].size() % 2)) {
     for (int i = 0; i < Vert[current_index].size(); i += 2) {
 
       // only draw line segment if we have a PAIR of verts (this will NOT be the case while a new segment is being drawn) 
+
       if (i + 1 < Vert[current_index].size())
 	line(draw_im, Vert[current_index][i], Vert[current_index][i + 1], Scalar(0, g, r), 2);
     }
@@ -508,18 +599,25 @@ void draw_overlay()
 
 //----------------------------------------------------------------------------
 
+// write which images have been marked as "bad" -- i.e., they contain no trail
+
 void saveBad()
 {
   FILE *fp = fopen("bad.txt", "w");
   for (int i = 0; i < dir_image_filename.size(); i++) {
-    bool bad = (Bad_idx_set.find(i) != Bad_idx_set.end());
-    fprintf(fp, "%i: %i\n", i, bad);
+    fprintf(fp, "%i: %i\n", i, isBad(i));
     fflush(fp);
   }
   fclose(fp);
 }
 
 //----------------------------------------------------------------------------
+
+// read file that specifies which images should be ignored because they contain
+// no trail or the trail geometry does not conform to assumptions
+
+// this assumes image filenames have already been loaded, so we know how many lines
+// should be in this file
 
 void loadBad()
 {
@@ -530,13 +628,33 @@ void loadBad()
     fscanf(fp, "%i: %i\n", &i, &bad);
     if (bad)
       Bad_idx_set.insert(i);
-    //    printf("%i: %i\n", i, bad);
   }
   fclose(fp);
 
 }
 
 //----------------------------------------------------------------------------
+
+// check whether a given image is contained in current set of "bad" (non-trail") images
+
+bool isBad(int idx)
+{
+  return (Bad_idx_set.find(idx) != Bad_idx_set.end());
+}
+
+//----------------------------------------------------------------------------
+
+// check whether a given image is contained in set of those with ground-truth vertices
+// specified
+
+bool isVert(int idx)
+{
+  return (Vert_idx_set.find(idx) != Vert_idx_set.end());
+}
+
+//----------------------------------------------------------------------------
+
+// write all current ground-truth trail vertices to file
 
 // requires that an image have exactly 4 verts to be written
 
@@ -560,13 +678,15 @@ void saveVert()
   }
   printf("saved %i images with verts [%i S + %i U = %i]\n", 
 	 num_saved_verts, 
-	 Vert_idx_set.size(),
-	 NoVert_idx_set.size(),
-	 Vert.size());
+	 (int) Vert_idx_set.size(),
+	 (int) NoVert_idx_set.size(),
+	 (int) Vert.size());
   fclose(fp);
 }
 
 //----------------------------------------------------------------------------
+
+// utility function for reading ground-truth vertices from file
 
 // extract next integer from string starting from index startPos
 // if s[startPos] is NOT a digit, keeps searching until one is found
@@ -589,10 +709,11 @@ bool getNextInt(string s, int startPos, int & curPos, int & nextInt)
   }
   else
     return false;
-
 }
 
 //----------------------------------------------------------------------------
+
+// utility function for reading ground-truth coordinate pair (x, y) from file
 
 bool getNextVert(string s, int startPos, int & curPos, int & x, int & y)
 {
@@ -612,6 +733,120 @@ bool getNextVert(string s, int startPos, int & curPos, int & x, int & y)
 }
 
 //----------------------------------------------------------------------------
+
+// how many "images away" from this one is the nearest image for which we have ground-truth?
+
+// i should be the index of a nonvert image
+
+void calculate_closest_vert_dist(int i)
+{
+  int j, last_dist, next_dist;
+
+  // get NEXT vert index
+  
+  for (j = i + 1, next_dist = NO_DISTANCE; j < Vert.size() && (isBad(j) || !isVert(j)); j++)
+    ;
+  
+  if (j >= 0 && j < Vert.size() && isVert(j)) 
+    next_dist = j - i;
+  
+  // get LAST vert index 
+  
+  for (j = i - 1, last_dist = NO_DISTANCE; j >= 0 && (isBad(j) || !isVert(j)); j--)
+    ;
+  if (j >= 0 && j < Vert.size() && isVert(j)) 
+    last_dist = i - j;
+  
+  // is forward or backward dist smaller?
+  
+  // at least one distance is "infinite"
+  
+  if (next_dist == NO_DISTANCE) 
+    ClosestVert_dist[i] = last_dist;
+  else if (last_dist == NO_DISTANCE) 
+    ClosestVert_dist[i] = next_dist;
+  
+  // both distances normal
+  
+  else {
+    if (next_dist <= last_dist) 
+      ClosestVert_dist[i] = next_dist;
+    else 
+      ClosestVert_dist[i] = last_dist;
+  }
+}
+
+//----------------------------------------------------------------------------
+
+// a new vert has been inserted.  recalculate MINIMUM number of distances from 
+// non-vert images to nearest vert image
+
+int most_isolated_nonvert_image_idx(int new_vert_idx)
+{
+  int i, j, max_dist, max_dist_idx;
+
+  // inserted vertex is now out of the game
+
+  ClosestVert_dist[new_vert_idx] = NO_DISTANCE;
+
+  // forward non-verts
+
+  for (j = new_vert_idx + 1; j < Vert.size() && !isVert(j); j++)
+    if (!isBad(j)) 
+      calculate_closest_vert_dist(j);
+
+  // backward non-verts
+
+  for (j = new_vert_idx - 1; j >= 0 && !isVert(j); j--)
+    if (!isBad(j)) 
+      calculate_closest_vert_dist(j);
+
+  // now find global max
+
+  for (i = 0, max_dist = 0, max_dist_idx = NO_INDEX; i < Vert.size(); i++) 
+    if (ClosestVert_dist[i] != NO_DISTANCE && ClosestVert_dist[i] > max_dist) {
+      max_dist = ClosestVert_dist[i];
+      max_dist_idx = i;
+    }
+
+  max_closest_vert_dist = max_dist;
+
+  return max_dist_idx;
+}
+
+
+//----------------------------------------------------------------------------
+
+// calculate distances from ALL non-vert images to nearest vert image.  
+// expensive, but should be called only once, when program is initialized
+
+int most_isolated_nonvert_image_idx()
+{
+  int i, j, last_dist, next_dist, max_dist, max_dist_idx;
+
+  
+  for (i = 0; i < Vert.size(); i++) {
+    
+    if (!isBad(i) && !isVert(i)) 
+      calculate_closest_vert_dist(i);
+    else
+      ClosestVert_dist[i] = NO_DISTANCE;
+  }
+
+  for (i = 0, max_dist = 0, max_dist_idx = NO_INDEX; i < Vert.size(); i++) 
+    if (ClosestVert_dist[i] != NO_DISTANCE && ClosestVert_dist[i] > max_dist) {
+      max_dist = ClosestVert_dist[i];
+      max_dist_idx = i;
+    }
+
+  max_closest_vert_dist = max_dist;
+
+  return max_dist_idx;
+}
+
+//----------------------------------------------------------------------------
+
+// get all ground-truth trail vertices from file
 
 // assumes Vert vector is already the right size, so that vertices may be added
 // at the appropriate indices
@@ -635,6 +870,11 @@ int loadVert()
 
   while (getline(inStream, line)) {
 
+    // skip comments
+
+    if (line[0] == COMMENT_CHAR)
+      continue;
+
     // get image index
 
     if (!getNextInt(line, 0, line_idx, image_idx)) {
@@ -649,8 +889,6 @@ int loadVert()
     if (iter != NoVert_idx_set.end())
       NoVert_idx_set.erase(iter);
 
-    //    printf("index %i (%i)\n", image_idx, line_idx);
-
     // get x, y coords of vert
 
     while (getNextVert(line, line_idx, line_idx, x_val, y_val)) {
@@ -658,15 +896,14 @@ int loadVert()
       v.x = x_val;
       v.y = y_val;
       Vert[image_idx].push_back(v);
-
-      //      printf("  (%i, %i)\n", x_val, y_val);
     }
 
     num_verts++;
   }
  
-
   inStream.close();
+
+  next_nonvert_idx = most_isolated_nonvert_image_idx();
 
   return num_verts;
 }
@@ -679,12 +916,12 @@ int main( int argc, const char** argv )
 
   printf("hello, trail!\n");
 
-  // initialize
+  // initialize for LINEAR trail approximation (quadrilateral)
 
-  trailEdgeRow.push_back(100);
-  trailEdgeRow.push_back(175);
+  trailEdgeRow.push_back(IMAGE_ROW_FAR);
+  trailEdgeRow.push_back(IMAGE_ROW_NEAR);
 
-  add_all_images();
+  add_all_images_from_file("imagedirs.txt");
 
   // create initial, ordered indices 
 
@@ -711,9 +948,12 @@ int main( int argc, const char** argv )
   printf("%i bad\n", (int) Bad_idx_set.size());
 
   Vert.resize(Random_idx.size());
+  ClosestVert_dist.resize(Random_idx.size());   
 
   num_saved_verts = loadVert();
   printf("%i with verts\n", num_saved_verts);
+
+  set_current_index(ZERO_INDEX);
 
   // display
 
